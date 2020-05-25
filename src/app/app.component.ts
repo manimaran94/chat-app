971536a724e6cb8401a40a7395d64c08ed1238fb
unknown <mani.maran@ameexusa.com>
2020-05-25 08:55:52 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public ELEMENT_DATA: Array<any> = [
+    { name: 'User1', company: 'Company-A', bgColor:'#ff00ff'},
+    { name: 'User2', company: 'Company-B', bgColor:'#ff0080' },
+    { name: 'User3', company: 'Company-C', bgColor:'#8000ff' },
+    { name: 'User4', company: 'Company-D', bgColor:'#bf00ff' },
+    { name: 'User5', company: 'Company-E', bgColor:'#00ffbf' },
+    { name: 'User6', company: 'Company-F', bgColor:'#80ff00' },
+  ];
+  public selection = new SelectionModel<any>(false);
+  public selectedUser = new FormControl('');
+  public messageText = new FormControl('');
+  public messages = {};
+  public displayedColumns: string[] = ['select', 'name', 'weight', 'chat'];
+  public dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  ngOnInit() {
+
+  }
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+  public filterList() {
+    this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+    this.dataSource.data = this.dataSource.data.filter(
+      (data) => data.name !== this.selectedUser.value
+    )
+  }
+  public sendMessage() {
+    if (this.messages[this.selectedUser.value + '-' + this.selection.selected[0].name]) {
+      this.messages[this.selectedUser.value + '-' + this.selection.selected[0].name].push(
+        {
+          name: this.selectedUser.value,
+          message: this.messageText.value
+        }
+      );
+    } else if (this.messages[this.selection.selected[0].name + '-' + this.selectedUser.value]) {
+      this.messages[this.selection.selected[0].name + '-' + this.selectedUser.value].push(
+        {
+          name: this.selectedUser.value,
+          message: this.messageText.value
+        });
+    } else {
+      this.messages[this.selectedUser.value + '-' + this.selection.selected[0].name] = [{
+        name: this.selectedUser.value,
+        message: this.messageText.value
+      }];
+    }
+    this.messageText.setValue('');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatTableModule} from '@angular/material/table';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { 
+  MatCheckboxModule,
+  MatCardModule,
+  MatSelectModule,
+  MatInputModule,
+  MatButtonModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    FlexLayoutModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
